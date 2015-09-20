d6a953f283541bcc623155bb668d9ba561ec12fc
Artyom Vybornov <art-vybor@ya.ru>
2015-09-21 00:09:35 +0300

    modeling add lab6 report

--- modeling/lab6/trampoline.py
+++ modeling/lab6/trampoline.py
@@ -1,17 +1,20 @@
+from math import sqrt
+
 m = 80
 k = 500
-F_jump = 240
+E_jump = 240
 n = 10
 
 g = 9.81
 
 h = lambda dx: k*dx**2*1.0/(2*m*g)
-new_dx = lambda dx, F_jump: dx+F_jump*1.0/k
-
+new_dx = lambda dx: sqrt(dx**2+E_jump*2.0/k)
 
 dx = 0
-for i in range(1,n+1):
-    dx = new_dx(dx, F_jump)
+for i in range(1, n+1):
+    dx = new_dx(dx)
 
     print 'h(%s) = %s' % (i, h(dx))
+
+
 
