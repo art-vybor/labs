c92e07dfff0d7bb11e68912d2ac7aac5284b1659
Artyom Vybornov <art-vybor@ya.ru>
2015-09-22 23:28:15 +0300

    lab6 modeling rewrite

--- modeling/lab6/trampoline.py
+++ modeling/lab6/trampoline.py
@@ -1,20 +1,22 @@
-from math import sqrt
+import numpy as np
+from scipy.optimize import fsolve
+
 
 m = 80
-k = 500
+k = 5000
 E_jump = 240
-n = 10
+n = 20
 
 g = 9.81
 
+f = lambda x: -1/(x-1) + k - 1
+F = lambda dx, dx1: f(dx1)*dx1**2 - f(dx)*dx**2 - 2*E_jump
+
 h = lambda dx: k*dx**2*1.0/(2*m*g)
-new_dx = lambda dx: sqrt(dx**2+E_jump*2.0/k)
+new_dx = lambda dx: min(fsolve(lambda dx1: F(dx, dx1), max(dx, 1e-5))[0], 1-1e-5)
 
 dx = 0
 for i in range(1, n+1):
     dx = new_dx(dx)
 
-    print 'h(%s) = %s' % (i, h(dx))
-
-
-
+    print '(%s,%s)' % (i, h(dx))
