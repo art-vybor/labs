951262410c94ff4a76e36f60b00ea00d2fa9c894
Artyom Vybornov <art-vybor@ya.ru>
2015-12-15 14:15:55 +0400

    modeling lab4 report

--- modeling/lab4/doit.py
+++ modeling/lab4/doit.py
@@ -6,20 +6,9 @@ import math
 import pandas
 
 bins_for_histogram = 20
-def build_probability_dict(f):
-    P = {}
-    for x in f:
-        if x not in P:
-            P[x] = 0
-        P[x] += 1
-
-    return {k:v*1.0/len(f) for k,v in P.iteritems()}
-
 
 def get_quality(f1, f2):
     def get_mean_for_interval(f, start, end, first=False):
-
-        #print f, start, end
         summ = 0
         num = 0
         for y in f:
@@ -34,18 +23,14 @@ def get_quality(f1, f2):
             return summ*1.0/num
         return 0
 
-    P1 = build_probability_dict(f1).values()    
-    P2 = build_probability_dict(f2).values()
-    
+    P1 = f1
+    P2 = f2 
+   
     start = min(min(P1), min(P2))
     end = max(max(P1), max(P2))    
     step = (end-start)/(bins_for_histogram+1)
     prev_start = start
     start += step
-
-    #print start, end
-
-
     n1 = []
     n2 = []
     first = True
@@ -58,9 +43,7 @@ def get_quality(f1, f2):
 
         prev_start = start
         start += step
-    print n1
-    print n2
-    return max(abs(np.array(n1) - np.array(n2)))
+    return n1, n2, sum(abs(np.array(n1) - np.array(n2)))
 
 
 metro = pandas.read_csv('data_praga_metro_1.csv', sep=',')
@@ -76,12 +59,48 @@ for station in stations:
 
 divider = max(total_a+total_b)
 
-# total_a = [x*1.0/divider for x in total_a]
-# total_b = [x*1.0/divider for x in total_b]
+total_a = [x*1.0/divider for x in total_a]
+total_b = [x*1.0/divider for x in total_b]
 
+a_60,b_60 = [],[]
+f = []
 for i in range(0, len(total_a)-1):
     a = total_a[:i] + total_a[i+1:]
     b = total_b[:i] + total_b[i+1:]
-    #print i, get_quality(a, b)
+    if i == 60:
+        a_60 = a
+        b_60 = b
+    x,y,q = get_quality(a, b)
+    f.append(q)
 
-print get_quality(total_a, total_b)
+    print int(a[i]*divider), int(b[i]*divider), q 
+
+print 'min', min(f)
+print 'max', max(f)
+print total_a[60]*divider, total_b[60]*divider
+
+
+def draw(a,b):
+    def add_plot(a, clr):
+        x,y=[],[]
+        i = 0
+        while i < len(a):
+            x.append(i)
+            x.append(i+1)
+            y.append(a[i])
+            y.append(a[i])
+            i+=1
+            plt.plot(x,y,color=clr)
+            x,y = [],[]
+            
+    add_plot(a,'b')
+    add_plot(b,'g')
+    plt.show()
+
+x,y,q = get_quality(total_a, total_b)
+print 'total:',q
+draw(x,y)
+
+x,y,q = get_quality(a_60, b_60)
+print 'total:',q
+draw(x,y)
