e906895013c81949336164ebe9542834ee8fbcb8
Artyom Vybornov <art-vybor@ya.ru>
2016-01-10 01:16:21 +0300

    optimization lab3 code

---
+++ optimization/lab3/3_1_penalty/doit.py
@@ -0,0 +1,34 @@
+# penalty method
+
+from scipy import optimize
+
+r = 1e-5
+eps = 1e-9
+C = 4
+
+f = lambda (x1, x2): (x1**2 - 2)**2 + x2**2 - 1
+
+g1 = lambda (x1, x2): -(x1+1)**2 + 3
+g2 = lambda (x1, x2): (x1+x2)**2 - 2
+g = lambda g_j, x: max(g_j(x), 0)
+
+P = lambda x, r: r*1.0/2 * (g(g1, x)**2 + g(g2, x)**2)
+
+F = lambda x, r: f(x) + P(x, r)
+
+def F(x, rk):
+    x = tuple(x)
+    return f(x) + P(x, rk)
+
+x = (0,0)
+print 'x0: %s' % list(x)
+
+while True:
+    x = tuple(optimize.minimize(lambda x: F(x, r), x).x)
+    print 'x = %s, penalty: %s, ' % (list(x), P(x, r))
+    if not P(x, r) <= eps:
+        r *= C
+    else:
+        break
+
+print 'result: %s' % list(x)
---
+++ optimization/lab3/3_2_lagrange/doit.py
@@ -0,0 +1,34 @@
+from scipy import optimize
+
+r = 1e-5
+eps = 1e-9
+C = 4
+mu = (0, 0)
+
+f = lambda (x1, x2): (x1**2 - 2)**2 + x2**2 - 1
+
+g1 = lambda (x1, x2): -(x1+1)**2 + 3
+g2 = lambda (x1, x2): (x1+x2)**2 - 2
+g = [g1, g2]
+
+P = lambda x, r: sum(max(0, mi + r * gi(x))**2 - mi**2 for mi, gi in zip(mu, g)) / (2*r)
+
+L = lambda x, r: f(x) + P(x, r)
+
+def F(x, rk):
+    x = tuple(x)
+    return f(x) + P(x, rk)
+
+x = (0,0)
+print 'x0: %s' % list(x)
+
+while True:
+    x = tuple(optimize.minimize(lambda x: F(x, r), x).x)
+    print 'x = %s, penalty: %s, ' % (list(x), P(x, r))
+    if not abs(P(x, r)) <= eps:
+        mu = [max(0, mi+r*gi(x)) for mi, gi in zip(mu, g)]
+        r *= C
+    else:
+        break
+
+print 'result: %s' % list(x)
